7739cc1e483163566b95f29894b443453dad794c
Jonathan G Rennison <j.g.rennison@gmail.com>
2014-02-21 20:28:07 +0000

    No longer #define NDEBUG if unset, this can clash with other headers, test for BTREE_DEBUG being defined instead.

--- btree.h
+++ btree.h
@@ -115,10 +115,6 @@
 #include <string>
 #include <utility>
 
-#ifndef NDEBUG
-#define NDEBUG 1
-#endif
-
 namespace btree {
 
 // Inside a btree method, if we just call swap(), it will choose the
@@ -690,17 +686,17 @@ class btree_node {
     f->max_count = max_count;
     f->count = 0;
     f->parent = parent;
-    if (!NDEBUG) {
+#ifdef BTREE_DEBUG
       memset(&f->values, 0, max_count * sizeof(value_type));
-    }
+#endif
     return n;
   }
   static btree_node* init_internal(internal_fields *f, btree_node *parent) {
     btree_node *n = init_leaf(f, parent, kNodeValues);
     f->leaf = 0;
-    if (!NDEBUG) {
+#ifdef BTREE_DEBUG
       memset(f->children, 0, sizeof(f->children));
-    }
+#endif
     return n;
   }
   static btree_node* init_root(root_fields *f, btree_node *parent) {
